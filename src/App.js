46f93a826fe44ea2cd68df0ec6d25408ec8af013
chetan chumble <chetan.chumble@maximess.com>
2022-09-19 15:09:59 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Login from "./pages/login";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Main from "./pages/main";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Login />}></Route>
+        <Route path="/" exact element={<Main />}></Route>
       </Routes>
     </Router>
   );
